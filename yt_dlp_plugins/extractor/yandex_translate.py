b6bae12b500d7d9652e691fcbfc4521d2853a49a
Bykov Alexey <gnfalex@rambler.ru>
2024-01-16 18:32:09 +0200

    second rework

--- yt_dlp_plugins/extractor/yandex_translate.py
+++ yt_dlp_plugins/extractor/yandex_translate.py
@@ -4,35 +4,46 @@ import yandex
 
 from yt_dlp.extractor.common import InfoExtractor
 from yt_dlp.extractor.youtube import YoutubeIE
+from yt_dlp.extractor.vk import VKIE
 
-def getYT (url, dld):
-  test = YoutubeIE()
-  test.set_downloader(dld)
-  data = test.extract(url)
-
+IEList = [YoutubeIE, VKIE]
 
 class YandexTranslateIE(InfoExtractor):
     _WORKING = False
-    _VALID_URL = r'.*'
+    _VALID_URL = r'^YT:.*'
     _ENABLED = False
-
 
     def __init__(self, **kwargs):
         self._kwargs = kwargs
 
+    @classmethod
+    def suitable(cls, url):
+      for ie in IEList:
+        if ie.suitable(url):
+          return True
+      return False
+
+    @classmethod
+    def _match_id(cls, url):
+      for ie in IEList:
+        if ie.suitable(url):
+          return ie._match_id(url)
+      return False
+
     def _real_extract(self, url):
-        self.to_screen('URL "%s" successfully captured' % url)
-        if YoutubeIE.suitable(url):
-          tmpIE = YoutubeIE()
-          tmpIE.set_downloader(self._downloader)
-          video_id = tmpIE._match_id(url)
-          info = tmpIE.extract(url)
-          vidTr = yandex.requestVideoTranslation(self, f'https://youtu.be/{video_id}', video_id)
-          if vidTr.status == 2:
-            self.report_warning(f"Video translate delayed. Please wait {vidTr.remainingTime}s")
-          subTr = yandex.requestSubtitlesTranslation(self, f'https://youtu.be/{video_id}', video_id)
-        else:
-          raise Exception
+        info = None
+        for ie in IEList:
+          if ie.suitable(url):
+            tmpIE = ie()
+            if 'Youtube' in ie.ie_key():
+              tmpIE.set_downloader(self._downloader)
+            video_id = tmpIE._match_id(url)
+            info = tmpIE.extract(url)
+            vidTr = yandex.requestVideoTranslation(self, f'https://youtu.be/{video_id}' if 'Youtube' in ie.ie_key() else url, video_id)
+            if vidTr.status == 2:
+              self.report_warning(f"Video translate delayed. Please wait {vidTr.remainingTime}s")
+            subTr = yandex.requestSubtitlesTranslation(self, f'https://youtu.be/{video_id}' if 'Youtube' in ie.ie_key() else url, video_id)
+
         if info is None: info = { "_type": "video", "url":url, "id": video_id, "title": f"Yandex translation", "duration": vidTr.duration}
         if not "formats" in info:  info["formats"]=[]
         if not "subtitles" in info: info["subtitles"]={}
